d85cd984335555da6dcfc18d0e04c190aaa50d2d
Abhishek Raghav <115450330+abhiraghav23@users.noreply.github.com>
2024-11-27 11:29:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,21 +11,18 @@ function convertToRoman(num) {
 
   let result = "";
 
-  // Iterate over the Roman numeral mapping
   for (let i = 0; i < 7; i++) {
     const [symbol, value] = obj[i];
     
-    // Append as many of the current symbol as possible
     while (num >= value) {
       result += symbol;
       num -= value;
     }
     
-    // Handle subtractive notation
-    if (i < 6) { // Ensure there's a next smaller symbol to check
-      const [nextSymbol, nextValue] = obj[i + 2]; // Look two steps ahead (e.g., C for 900)
-      if (num >= value - nextValue) {
-        result += nextSymbol + symbol; // Append subtractive notation (e.g., 'CM')
+  
+    if (i < 6) { 
+      const [nextSymbol, nextValue] = obj[i + 2]; 
+        result += nextSymbol + symbol; 
         num -= (value - nextValue);
       }
     }
@@ -34,10 +31,8 @@ function convertToRoman(num) {
   return result;
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 console.log(convertToRoman(36));
 
-// do not edit below this line
 module.exports = convertToRoman;
 
